2904b8e9f200b5c3d53c4f877573f17b3e4586de
rasel <russelroy15@gmail.com>
2021-11-06 11:36:34 +0600

    WIP on Doctors Portal server

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kwsxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("database connect");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Doctors Portal!");
@@ -9,3 +31,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at Port ${port}`);
 });
+
+//ah6SQva#byJ^7YLq
