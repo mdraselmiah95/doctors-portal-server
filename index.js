9c333ae65446c7515d63e1e641ef622a9018b1b9
rasel <russelroy15@gmail.com>
2021-11-06 11:05:28 +0600

    WIP on Doctors Portal server

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Doctors Portal!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at Port ${port}`);
 });
