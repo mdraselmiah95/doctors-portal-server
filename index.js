9b07bf6589f6c8e0950262c3589a6f7824161b09
rasel <russelroy15@gmail.com>
2021-11-07 23:45:48 +0600

    new code added

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ async function run() {
 
     app.get("/appointments", async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      // const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
+      console.log(email, date);
       const cursor = appointmentsCollection.find(query);
       const appointments = await cursor.toArray();
       res.json(appointments);
