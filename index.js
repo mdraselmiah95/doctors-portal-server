5bcd90755ca3678fb9fb77585762c35636e60dbc
rasel <russelroy15@gmail.com>
2021-11-07 13:44:53 +0600

    WIP on doctors portal server

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ async function run() {
     const database = client.db("doctors_portal");
     const appointmentsCollection = database.collection("appointments");
 
+    app.get("/appointments", async (req, res) => {
+      //  const email = req.query.email;
+      //  const date = new Date(req.query.date).toLocaleDateString();
+
+      //  const query = { email: email, date: date };
+
+      const cursor = appointmentsCollection.find({});
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
     app.post("/appointments", async (req, res) => {
       const appointment = req.body;
       const result = await appointmentsCollection.insertOne(appointment);
