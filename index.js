8487cc98e4cfb42016573e4bcd06ca8b80932590
mdraselmiah95 <russelroy15@gmail.com>
2022-08-15 19:38:28 +0600

    WIP on Doctors Portal Server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lidezs8.mongodb.net/?retryWrites=true&w=majority`;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors Portal listening on port ${port}`);
+});
