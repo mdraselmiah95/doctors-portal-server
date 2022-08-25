215aad4ca047a677e7a33a8935dac4b4d4108fd6
mdraselmiah95 <russelroy15@gmail.com>
2022-08-25 20:00:34 +0600

    Doctors Portal Server Naming Convention

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ async function run() {
   try {
     await client.connect();
     // console.log("Database connected...");
+
     /**
      * API Naming Convention
      * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
      * app.get('/booking/:id') // get a specific booking
      * app.post('/booking') // add a new booking
      * app.patch('/booking/:id) //
+     * app.put('/booking/:id') // upsert ==> update (if exists) or insert (if doesn't exist)
+     * app.delete('/booking/:id) //
      */
 
     //Collection
