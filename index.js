3925081e70825c37d566153498aa1e170206b6cc
mdraselmiah95 <russelroy15@gmail.com>
2022-08-28 16:16:55 +0600

    WIP on Doctors Portal Server Admin Verify

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(users);
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     app.put("/users/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
